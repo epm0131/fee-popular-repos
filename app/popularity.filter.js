b3b4d197a8797ca9d9ee9d9ab84dedfa116cafdb
Ettore Merendino <merendino.ettore@gmail.com>
2016-12-12 18:58:21 -0500

    I fixed indentation

--- app/popularity.filter.js
+++ app/popularity.filter.js
@@ -1,32 +1,30 @@
 (function() {
   'use strict';
 
-angular.module('gh')
-  .filter('popularity', PopularityFilter);
-  console.log('am i being created?');
+  angular.module('gh')
+    .filter('popularity', PopularityFilter);
 
-function PopularityFilter() {
+  function PopularityFilter() {
 
-  return function sortByPopularity(input) {
+    return function sortByPopularity(input) {
 
-    var popularity = input.map(function calPopNum(repo) {
+      var popularity = input.map(function calPopNum(repo) {
 
-      var pop = repo.stargazers_count + (2*repo.forks_count)+(repo.open_issues_count/2);
-      repo.popularity = pop;
-      console.log('hello', pop);
-      return repo;
-    });
-    popularity = popularity.sort(function sortPopularity(a,b) {
-      if(a.popularity > b.popularity) {
-        return -1;
-      } else {
-        return 1;
-      }
+        var pop = repo.stargazers_count + (2*repo.forks_count)+(repo.open_issues_count/2);
+        repo.popularity = pop;
+        return repo;
+      });
 
-  });
-  return popularity;
+      popularity = popularity.sort(function sortPopularity(a,b) {
+        if(a.popularity > b.popularity) {
+          return -1;
+        } else {
+          return 1;
+        }
+
+      });
+      return popularity;
     };
   }
-
 
 }());
